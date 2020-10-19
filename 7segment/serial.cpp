ed14850deb85072d5002ea124159307449b1e745
MortalROs <15651883+MortalROs@users.noreply.github.com>
2020-10-19 16:38:14 +0300

    update

--- 7segment/serial.cpp
+++ 7segment/serial.cpp
@@ -1,28 +1,7 @@
 #define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
-
-#define USART_BAUDRATE 9600
-
-void serial_init ( unsigned short ubrr ) {
-	UBRR0 = ubrr ; // Set baud rate
-	UCSR0B |= (1 << TXEN0 ); // Turn on transmitter
-	UCSR0B |= (1 << RXEN0 ); // Turn on receiver
-	UCSR0C = (3 << UCSZ00 ); // Set for async . operation , no parity ,
-	// one stop bit , 8 data bits
-}
-
-void serial_out ( char ch )
-{
-	while (( UCSR0A & (1 << UDRE0 )) == 0);
-	UDR0 = ch ;
-}
-
-char serial_in ()
-{
-	while ( !( UCSR0A & (1 << RXC0 )) );
-	return UDR0 ;
-}
+#include "serial.h"
 
 char nrprint(char *dataprint) {
 	
