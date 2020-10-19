a80a148e83a55a0a30ed0636282b303c3fcc1d5c
MortalROs <15651883+MortalROs@users.noreply.github.com>
2020-10-19 15:26:55 +0300

    update

--- 7segment/serial.cpp
+++ 7segment/serial.cpp
@@ -26,37 +26,20 @@ char serial_in ()
 
 char nrprint(char *dataprint) {
 	
-	int printvalue = 0x00;
-	
-	if(*dataprint == '0') {
-		printvalue = 0x3F;
-	}	
-	if(*dataprint == '1') {
-		printvalue = 0x06;
-	}
-	if(*dataprint == '2') {
-		printvalue = 0x5B;
-	}
-	if(*dataprint == '3') {
-		printvalue = 0x4F;
-	}
-	if(*dataprint == '4') {
-		printvalue = 0x66;
-	}
-	if(*dataprint == '5') {
-		printvalue = 0x6D;
-	}
-	if(*dataprint == '6') {
-		printvalue = 0x7D;
-	}
-	if(*dataprint == '7') {
-		printvalue = 0x07;
-	}
-	if(*dataprint == '8') {
-		printvalue = 0x7F;
-	}
-	if(*dataprint == '9') {
-		printvalue = 0x6F;
+	int printvalue;
+
+	switch(*dataprint) {
+		case '0': printvalue = 0x3F; break;
+		case '1': printvalue = 0x06; break;
+		case '2': printvalue = 0x5B; break;
+		case '3': printvalue = 0x4F; break;
+		case '4': printvalue = 0x66; break;
+		case '5': printvalue = 0x6D; break;
+		case '6': printvalue = 0x7D; break;
+		case '7': printvalue = 0x07; break;
+		case '8': printvalue = 0x7F; break;
+		case '9': printvalue = 0x6F; break;
+		default: printvalue = 0x00; break;
 	}
 	
 	return printvalue;
